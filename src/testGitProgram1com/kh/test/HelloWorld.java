8fe8d07bf54bb666c6e179826888ddae8fc55f3c
eunju <eunju@DESKTOP-IE2PDJ4>
2019-04-22 14:47:20 +0900

    최초 프로젝트

---
+++ src/testGitProgram1com/kh/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package testGitProgram1com.kh.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("гою╖");
+
+	}
+
+}
